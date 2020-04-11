2f1481e53ece07fa470d35b61f2b4159e83020e3
maheswarreddy peram <38140817+MaheshwarReddyPeram1@users.noreply.github.com>
2020-04-11 22:46:34 +0530

    with some edits

---
+++ logindb.php
@@ -0,0 +1,24 @@
+<?php
+$id=$_POST["id"];
+$course1=$_POST["course1"];
+$course2=$_POST["course2"];
+$sum=$course1+$course2;
+$avg=$sum/200;
+$percentage=$avg*100;
+
+$conn = mysqli_connect('localhost','admin','admin','employee');
+
+if(!$conn)
+{
+die("connection failed:" .mysqli_connect_error());
+}
+$sql="INSERT INTO student VALUES ('$id','$course_1','$course_2','$percentage')");
+
+if(mysqli_query($conn,$sql)){
+echo "success";
+}
+else{
+echo:error;
+}
+
+?>
